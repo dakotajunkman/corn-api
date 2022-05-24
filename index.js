4afbe2c7f2bbaa8624cf006d5917210d6d0075fc
Dakota Junkman <dakota.junkman@gmail.com>
2022-05-24 11:22:31 -0500

    cannot get corn yet :(, but theres a picture

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+    res.render("welcome");
+})
+
+const PORT = process.env.PORT ?? 8080;
+app.listen(PORT, () => {
+    console.log(`Listening for corn on port ${PORT}`);
+});
+
+module.exports = app;
